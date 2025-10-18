fb566779f9bec27f5d5d7521ae2065a2d9a60957
Florin Varga <vargaflorin7@gmail.com>
2025-10-18 13:35:15 +0200

    - completed topic creation

---
+++ src/components/topics/topic-create-form.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import {
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Textarea,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+import { FormEvent, startTransition, useActionState } from "react";
+
+export default function TopicCreateForm() {
+  const [formState, action] = useActionState(actions.createTopic, {
+    errors: {},
+  });
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    startTransition(() => {
+      action(formData);
+    });
+  }
+
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form onSubmit={handleSubmit} noValidate>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create Topic</h3>
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(",")}
+            />
+            <Textarea
+              name="description"
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(",")}
+            />
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-200">
+                {formState.errors._form?.join(", ")}
+              </div>
+            ) : null}
+
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
