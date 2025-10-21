7d2c893ac80d9de08f4b13427217d5463b2f75dd
Florin Varga <vargaflorin7@gmail.com>
2025-10-21 23:54:50 +0200

    - added post creation

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,5 +1,91 @@
-'use server';
+"use server";
 
-export async function createPost() {
-    //TODO: revalidate the topic show page
+import type { Post, Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
+
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  topicSlug: string,
+  formState: CreatePostFormState,
+  formData: FormData,
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user || !session.user.id) {
+    return {
+      errors: {
+        _form: ["You must be signed in"],
+      },
+    };
+  }
+
+  const topic: Topic | null = await db.topic.findFirst({
+    where: {
+      slug: topicSlug,
+    },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ["Topic not found"],
+      },
+    };
+  }
+
+  let post: Post;
+  try {
+    post = await db.post.create({
+      data: {
+        topicId: topic.id,
+        content: result.data.content,
+        title: result.data.title,
+        userId: session.user.id,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Failed to create a post"],
+        },
+      };
+    }
+  }
+
+  revalidatePath(paths.topicShow(topicSlug));
+  redirect(paths.postShow(topicSlug, post.id));
 }
--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { FormEvent, startTransition, useActionState } from "react";
 import {
   Button,
   Input,
@@ -9,13 +10,19 @@ import {
   Textarea,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
-import { FormEvent, startTransition, useActionState } from "react";
 import FormButton from "@/components/common/form-button";
 
-export default function TopicCreateForm() {
-  const [formState, action, isPending] = useActionState(actions.createTopic, {
-    errors: {},
-  });
+interface PostCreateFormProps {
+  slug: string;
+}
+
+export default function PostCreateForm({ slug }: PostCreateFormProps) {
+  const [formState, action, isPending] = useActionState(
+    actions.createPost.bind(null, slug),
+    {
+      errors: {},
+    },
+  );
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -28,35 +35,34 @@ export default function TopicCreateForm() {
   return (
     <Popover placement="left">
       <PopoverTrigger>
-        <Button color="primary">Create a Topic</Button>
+        <Button color="primary">Create a Post</Button>
       </PopoverTrigger>
       <PopoverContent>
         <form onSubmit={handleSubmit} noValidate>
           <div className="flex flex-col gap-4 p-4 w-80">
-            <h3 className="text-lg">Create Topic</h3>
+            <h3 className="text-lg">Create a Post</h3>
             <Input
-              name="name"
-              label="Name"
+              name="title"
+              label="Title"
               labelPlacement="outside"
-              placeholder="Name"
-              isInvalid={!!formState.errors.name}
-              errorMessage={formState.errors.name?.join(",")}
+              placeholder="Title"
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(", ")}
             />
             <Textarea
-              name="description"
-              label="Description"
+              name="content"
+              label="Content"
               labelPlacement="outside"
-              placeholder="Describe your topic"
-              isInvalid={!!formState.errors.description}
-              errorMessage={formState.errors.description?.join(",")}
+              placeholder="Content"
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(", ")}
             />
             {formState.errors._form ? (
-              <div className="rounded p-2 bg-red-200 border border-red-200">
+              <div className="rounded p-2 bg-red-200 border border-red-400">
                 {formState.errors._form?.join(", ")}
               </div>
             ) : null}
-
-            <FormButton isLoading={isPending}>Save</FormButton>
+            <FormButton isLoading={isPending}>Create Post</FormButton>
           </div>
         </form>
       </PopoverContent>
